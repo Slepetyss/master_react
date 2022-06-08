0c9a0d7c97a6947d39978fdc739eb80fa504d2ea
Slepetyss <lucas.slepetys@hotmail.com>
2022-06-08 23:04:56 +0200

    Third commit

--- src/App.js
+++ src/App.js
@@ -2,31 +2,62 @@ import * as React from 'react';
 
 const title = 'React';
 
-const welcome = {
-  title: 'React',
-  greeting: 'Hey',
-};
-
-const myElement = (
-  <h1>
-    {welcome.greeting} {getTitle('title')} user!
-  </h1>
-);
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 const jsElement = React.createElement('h1', null, `Hello ${title}`);
 
-function getTitle(title) {
-  return welcome.title;
+//! This is a new component
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item['objectID']}>
+            <span>
+              <a href={item['url']}>{item['title']}</a>
+            </span>
+            <span>{item['author']}</span>
+            <span>{item['num_comments']}</span>
+            <span>{item['points']}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor='Search'>Search: </label>
+      <input type='search' id='search'></input>
+    </div>
+  );
 }
 
 function App() {
   return (
     <div>
-      {myElement}
-      ------
       {jsElement}
-      <label htmlFor='Search'>Search: </label>
-      <input type='search' id='search'></input>
+      <Search />
+      <List />
     </div>
   );
 }
