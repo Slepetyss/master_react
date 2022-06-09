af35e012bc05c98df204a30bcc19907561c8baf3
Slepetyss <lucas.slepetys@hotmail.com>
2022-06-09 14:49:39 +0200

    Fouth Commit / search term + event handler + state hooks

--- src/App.js
+++ src/App.js
@@ -2,64 +2,84 @@ import * as React from 'react';
 
 const title = 'React';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const jsElement = React.createElement('h1', null, `Hello ${title}`);
 
-//! This is a new component
-function List() {
+const Item = (props) => {
   return (
-    <ul>
-      {list.map(function (item) {
-        return (
-          <li key={item['objectID']}>
-            <span>
-              <a href={item['url']}>{item['title']}</a>
-            </span>
-            <span>{item['author']}</span>
-            <span>{item['num_comments']}</span>
-            <span>{item['points']}</span>
-          </li>
-        );
-      })}
-    </ul>
+    <li key={props.key}>
+      <span>
+        <a href={props.item['url']}>{props.item['title']}</a>
+      </span>
+      <span>{props.item['author']}</span>
+      <span>{props.item['num_comments']}</span>
+      <span>{props.item['points']}</span>
+    </li>
   );
-}
+};
 
-function Search() {
+const List = (props) => (
+  <ul>
+    {props.list.map(function (item) {
+      return <Item key={item['objectsID']} item={item} />;
+    })}
+  </ul>
+);
+
+const Search = (props) => {
+  const handleChange = (event) => {
+    props.onSearch(event);
+  };
+
   return (
     <div>
       <label htmlFor='Search'>Search: </label>
-      <input type='search' id='search'></input>
+      <input type='text' onChange={handleChange} id='search'></input>
+      <p>
+        Searching for <strong>{props.searchTerm}</strong>
+      </p>
     </div>
   );
-}
+};
 
-function App() {
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+    //updates state from search component
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter((story) => {
+    return story['title'].toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
     <div>
       {jsElement}
-      <Search />
-      <List />
+      <Search onSearch={handleSearch} searchTerm={searchTerm} />
+      <List list={searchedStories} />
     </div>
   );
-}
+};
 
 export default App;
